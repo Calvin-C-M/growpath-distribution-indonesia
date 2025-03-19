25d8caf714961e607e8f1dfa93e525c2dfcf7dd3
CalvinCM <montolalu28@gmail.com>
2025-03-20 00:11:34 +0700

    feat: add gdi_base for hiding unwanted fields

---
+++ gdi_base/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GDI Base',
+    'version': '18.1.0',
+    'author': 'Calvin',
+    'category': 'Accounting',
+    'website': 'https://example.com',
+    'summary': 'Base Customization for Growpath Distribution Indonesia',
+    'license': 'LGPL-3',
+    'description': """
+    """,
+    'depends': [
+        'base',
+        'purchase',
+        'stock',
+    ],
+    'data': [
+        'views/purchase_order_views_inherit.xml',
+        'views/stock_picking_views_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+    "assets": {
+
+    }
+}
