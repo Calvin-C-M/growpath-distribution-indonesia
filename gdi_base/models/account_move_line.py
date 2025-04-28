3b859e9e91e910ccaea1cdabad93a2be79587ee2
CalvinCM <montolalu28@gmail.com>
2025-04-28 11:24:34 +0700

    fix: account move line singleton

--- gdi_base/models/account_move_line.py
+++ gdi_base/models/account_move_line.py
@@ -7,20 +7,23 @@ class AccountMoveLine(models.Model):
 
     @api.constrains('quantity', 'purchase_line_id')
     def _validate_quantity(self):
-        if self.purchase_line_id and not self.purchase_line_id.is_downpayment and self.quantity > self.purchase_line_id.product_qty:
-            raise ValidationError('Quantity cannot be greater than demand quantity')
+        for rec in self:
+            if rec.purchase_line_id and not rec.purchase_line_id.is_downpayment and self.quantity > self.purchase_line_id.product_qty:
+                raise ValidationError('Quantity cannot be greater than demand quantity')
         
     @api.constrains('move_id')
     def _validate_purchase_lines(self):
-        if self in self.move_id.invoice_line_ids:
-            source_orders = self.move_id.invoice_line_ids.mapped('purchase_order_id')
-            if source_orders and not self.purchase_line_id:
-                raise ValidationError('Purchase line must be part of the purchase order')
+        for rec in self:
+            if rec in rec.move_id.invoice_line_ids:
+                source_orders = rec.move_id.invoice_line_ids.mapped('purchase_order_id')
+                if source_orders and not rec.purchase_line_id:
+                    raise ValidationError('Purchase line must be part of the purchase order')
 
     @api.constrains('move_id')
     def _validate_sale_lines(self):
-        if self in self.move_id.invoice_line_ids:
-            source_orders = self.move_id.invoice_line_ids.sale_line_ids.order_id
-            if source_orders and not self.purchase_line_id:
-                raise ValidationError('Sale line must be part of the purchase order')
+        for rec in self:
+            if rec in rec.move_id.invoice_line_ids:
+                source_orders = rec.move_id.invoice_line_ids.sale_line_ids.order_id
+                if source_orders and not rec.purchase_line_id:
+                    raise ValidationError('Sale line must be part of the purchase order')
         
