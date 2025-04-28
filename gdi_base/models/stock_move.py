749546ca384e392525c9ef0ce0b97c77ff6ba768
CalvinCM <montolalu28@gmail.com>
2025-04-28 10:48:09 +0700

    fix: stock move error

--- gdi_base/models/stock_move.py
+++ gdi_base/models/stock_move.py
@@ -7,19 +7,22 @@ class StockMove(models.Model):
 
     @api.constrains('quantity', 'product_uom_qty')
     def _validate_demand_quantity(self):
-        if self.quantity > self.product_uom_qty:
-            raise ValidationError('Quantity cannot be greater than demand quantity')
+        for rec in self:
+            if rec.quantity > rec.product_uom_qty:
+                raise ValidationError('Quantity cannot be greater than demand quantity')
         
     @api.constrains('picking_id')
     def _validate_picking_purchase_lines(self):
-        if self.picking_id.purchase_id:
-            purchase_lines = self.picking_id.purchase_id.order_line.ids
-            if not self.purchase_line_id or self.purchase_line_id.id not in purchase_lines:
-                raise ValidationError('Purchase line must be part of the purchase order')
+        for rec in self:
+            if rec.picking_id.purchase_id:
+                purchase_lines = rec.picking_id.purchase_id.order_line.ids
+                if not rec.purchase_line_id or rec.purchase_line_id.id not in purchase_lines:
+                    raise ValidationError('Purchase line must be part of the purchase order')
         
     @api.constrains('picking_id')
     def _validate_picking_sale_lines(self):
-        if self.picking_id.sale_id:
-            sale_lines = self.picking_id.sale_id.order_line.ids
-            if not self.sale_line_id or self.sale_line_id.id not in sale_lines:
-                raise ValidationError('Sales line must be part of the purchase order')
+        for rec in self:
+            if rec.picking_id.sale_id:
+                sale_lines = rec.picking_id.sale_id.order_line.ids
+                if not rec.sale_line_id or rec.sale_line_id.id not in sale_lines:
+                    raise ValidationError('Sales line must be part of the purchase order')
