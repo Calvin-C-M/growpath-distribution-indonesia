8ce30d8ab8b34d553b48925544f2188d4bceb203
CalvinCM <montolalu28@gmail.com>
2025-05-28 18:29:41 +0700

    fix: sequence on invoice and vendor bills when updating journal and unlinking record

--- gdi_base/models/account_move.py
+++ gdi_base/models/account_move.py
@@ -15,9 +15,11 @@ class AccountMove(models.Model):
             move_has_name = move.name and move.name != '/'
 
             if move.move_type == 'out_invoice' and not move_has_name:
-                move.name = self.env['ir.sequence'].next_by_code('account.move.invoice') or '/'
+                sequence = self.env.ref('gdi_base.account_move_invoice')
+                move.name = sequence.get_next_char(sequence.number_next_actual)
             elif move.move_type == 'in_invoice' and not move_has_name:
-                move.name = self.env['ir.sequence'].next_by_code('account.move.vendor.bill') or '/'
+                sequence = self.env.ref('gdi_base.account_move_vendor_bill')
+                move.name = sequence.get_next_char(sequence.number_next_actual)
             else:
                 if not move.posted_before and not move._sequence_matches_date():
                     # The name does not match the date and the move is not the first in the period:
@@ -28,3 +30,25 @@ class AccountMove(models.Model):
                     move._set_next_sequence()
                     
         self._inverse_name()
+        
+    @api.onchange('journal_id')
+    def _onchange_journal_id(self):
+        excluded_move_types = ['in_invoice', 'out_invoice']
+        if self.move_type not in excluded_move_types:
+            super()._onchange_journal_id()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'name' in vals and 'move_type' in vals:
+                if vals['move_type'] == 'out_invoice':
+                    sequence = self.env.ref('gdi_base.account_move_invoice')
+                    if vals['name'] == sequence.get_next_char(sequence.number_next_actual):
+                        vals['name'] = self.env['ir.sequence'].next_by_code('account.move.invoice')
+                elif vals['move_type'] == 'in_invoice':
+                    sequence = self.env.ref('gdi_base.account_move_vendor_bill')
+                    if vals['name'] == sequence.get_next_char(sequence.number_next_actual):
+                        vals['name'] = self.env['ir.sequence'].next_by_code('account.move.vendor.bill')
+
+        return super().create(vals_list)
+        
